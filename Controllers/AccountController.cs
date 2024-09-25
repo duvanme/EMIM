b536336d23f2d94ed6b1c942a37290f1c074970e
Duvan Mendoza <duvanpez12@hotmail.com>
2024-09-24 19:11:27 -0500

    Added Login and Register view and controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMIM.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: AccountController
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}

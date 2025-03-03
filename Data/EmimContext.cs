2fb90cf0ef8f8d0d2645ea665b1459f964c3aef4
Duvan Mendoza <duvanme1@gmail.com>
2025-03-02 21:51:23 -0500

    SeedAdminUser

--- Data/EmimContext.cs
+++ Data/EmimContext.cs
@@ -1,13 +1,19 @@
 using EMIM.Models;
+using EMIM.Views;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EMIM.Data;
 
 public class EmimContext : IdentityDbContext<User>
 {
-    public EmimContext(DbContextOptions<EmimContext> options) : base(options){}
+    private readonly IConfiguration configuration;
+    public EmimContext(DbContextOptions<EmimContext> options, IConfiguration configuration) : base(options)
+    {
+        this.configuration = configuration;
+    }
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Store> Stores { get; set; }
@@ -35,6 +41,36 @@ public class EmimContext : IdentityDbContext<User>
             new Category { Id = 2, Name = "Comida" },
             new Category { Id = 3, Name = "Tecnología" }
         );
+
+        var adminUser = new User
+        {
+            Id = Guid.NewGuid().ToString(), 
+            UserName = configuration["AdminUser:Email"],
+            NormalizedUserName = configuration["AdminUser:Email"]?.ToUpper(),
+            Email = configuration["AdminUser:Email"],
+            NormalizedEmail = configuration["AdminUser:Email"]?.ToUpper(),
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString(), 
+            FirstName = "Admin", 
+            LastName = "User",
+            Status = Status.Active, 
+            CreatedAt = DateTime.UtcNow, 
+            ModifiedAt = DateTime.UtcNow 
+        };
+
+        var passwordHasher = new PasswordHasher<User>();
+        adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, configuration["AdminUser:Password"]);
+
+        modelBuilder.Entity<User>().HasData(adminUser);
+
+        modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+            new IdentityUserRole<string>
+            {
+                RoleId = admin.Id,
+                UserId = adminUser.Id
+            }
+        );
+
 
     }
 }

b27dfd9f620527e0cb229b2e288a0ee623894cec
Julio <julianorozcol1504@gmail.com>
2025-03-10 07:18:15 -0500

    Vista de mis productos

---
+++ Views/Product/MyProducts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EMIM.Views.Product
+{
+    public class MyProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

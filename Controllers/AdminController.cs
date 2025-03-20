933f0c84320332672dc6d3a2ea497c9bfbccf1a8
xStevenLG <stloperag@gmail.com>
2025-03-19 19:27:53 -0500

    Solicitudes creacion de tienda a admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using EMIM.Data;
+using EMIM.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMIM.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly EmimContext context;
+
+        public AdminController(EmimContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult AdminNotification()
+        {
+            var stores = context.Stores
+            .Where(s => s.StoreStatus == 1)
+            .ToList();
+            return View(stores);
+        }
+
+        public IActionResult AdminProfile() => View();
+    }
+}

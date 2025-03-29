6f1c60a3391a93bf8b8b9615b7e1930d6afef927
duvanme <38926709+duvanme@users.noreply.github.com>
2025-03-29 17:58:06 -0500

    Update EmailService.cs

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -16,28 +16,7 @@ namespace EMIM.Services
         }
 
 
-        public async Task SendEmailAsync(string email, string subject, string message)
-        {
-            using (var smtpClient = new SmtpClient("smtp.gmail.com"))
-            {
-                smtpClient.Credentials = new NetworkCredential("4dm1nemim@gmail.com", "kcjuqxhhcjknifop");
-                smtpClient.EnableSsl = true;
-                smtpClient.Port = 587; // O el puerto que uses
-
-                var mailMessage = new MailMessage
-                {
-                    From = new MailAddress("4dm1nemim@gmail.com"),
-                    Subject = subject,
-                    Body = message,
-                    IsBodyHtml = true,
-                };
-                mailMessage.To.Add(email);
-
-                await smtpClient.SendMailAsync(mailMessage);
-            }
-        }
-
-       
+         
         public async Task<bool> SendEmailAsync(string toEmail, string subject, string message)
         {
             try
